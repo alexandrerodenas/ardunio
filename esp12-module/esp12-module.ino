0cebcb9e6dd9db10fccebd953995c0147b938400
Alexandre Rodenas <alexandre.rodenas@bioserenity.com>
2020-04-26 23:30:17 +0200

    split esp module in multiple file + deal with pump current state (not working)

--- esp12-module/esp12-module.ino
+++ esp12-module/esp12-module.ino
@@ -11,84 +11,7 @@
 
 ESP8266WebServer server ( 80 );
 
-
-void sendMeasuresOnWeb(int measureSensorA, int measureSensorB, int measureSensorC) {
-  // compute the required size
-  const size_t CAPACITY = JSON_ARRAY_SIZE(3);
-
-  // allocate the memory for the document
-  StaticJsonDocument<CAPACITY> doc;
-
-  // create an empty array
-  JsonArray array = doc.to<JsonArray>();
-
-  // add some values
-  array.add(measureSensorA);
-  array.add(measureSensorB);
-  array.add(measureSensorC);
-
-  sendJsonToWebClient(doc);
-}
-
-void sendJsonToWebClient(DynamicJsonDocument doc) {
-  char output[128];
-  serializeJson(doc, output);
-  server.send(200, "application/json", output);
-}
-
-
-DynamicJsonDocument readArduinoResponse() {
-  sendMeasureRequestToArduino();
-  DynamicJsonDocument doc(1024);
-
-  // Reading the response
-  boolean messageReady = false;
-  String message = "";
-  while (messageReady == false) {
-    if (Serial.available()) {
-      message = Serial.readString();
-      messageReady = true;
-    }
-  }
-  // Attempt to deserialize the JSON-formatted message
-  DeserializationError error = deserializeJson(doc, message);
-  if (error) {
-    Serial.print(F("deserializeJson() failed: "));
-    Serial.println(error.c_str());
-  }
-
-  return doc;
-}
-
-void sendPumpCommand() {
-  DynamicJsonDocument doc(1024);
-  doc["type"] = "command";
-  doc["subtype"] = "pump";
-  serializeJson(doc, Serial);
-}
-
-void runPumpProcess() {
-  sendPumpCommand();
-  DynamicJsonDocument doc = readArduinoResponse(); // IN PROGRESS
-  sendJsonToWebClient(doc);
-  doc = readArduinoResponse(); // DONE
-  sendJsonToWebClient(doc);
-}
-
-void sendMeasureRequestToArduino() {
-  DynamicJsonDocument doc(1024);
-  doc["type"] = "request";
-  serializeJson(doc, Serial);
-}
-
-void runMeasuresProcess() {
-  DynamicJsonDocument doc = readArduinoResponse();
-  const int humidity_sensorA = doc["sensorA"];
-  const int humidity_sensorB = doc["sensorB"];
-  const int humidity_sensorC = doc["sensorC"];
-
-  sendMeasuresOnWeb(humidity_sensorA, humidity_sensorB, humidity_sensorC);
-}
+int pumpState = 0; // 0 = NOT WORKING; 1 = IN PROGRESS
 
 void setup() {
   Serial.begin ( 9600 );
@@ -123,19 +46,35 @@ void setup() {
     server.sendHeader("Access-Control-Allow-Origin", "*");
     server.sendHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
     server.sendHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    runPumpProcess();
+    sendPumpCommand();
   });
 
 
+  server.on("/pump/state", HTTP_GET, []() {
+    server.sendHeader("Access-Control-Allow-Origin", "*");
+    server.sendHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+    server.sendHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    sendPumpState();
+  });
+  
   // Démarre le serveur web - Start Web Server
   server.begin();
   Serial.println ( "HTTP server started" );
-
 }
 
 
 void loop() {
   // Regularly checks the connection of new clients
   server.handleClient();
+  DynamicJsonDocument doc = readAsyncArduinoResponse();
+  if(doc["type"] == "response"){
+    if(doc["subtype"] == "pump"){
+      if(doc["status"] == "DONE"){
+        pumpState = 0;
+      } else {
+        pumpState = 1;
+      }
+    }
+  }
   delay(100);
 }
